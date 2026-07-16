cf5f46849a7b5a7565455b58af56ec9de6afabad
ezerssss <magbanuaezra@gmail.com>
2026-07-16 22:05:45 +0800

    Fetch GitHub data

---
+++ src/types/github.ts
@@ -0,0 +1,9 @@
+export type GitHubRepo = {
+    id: string;
+    name: string;
+    full_name: string;
+    html_url: string;
+    description: string;
+    created_at: string;
+    updated_at: string;
+};
