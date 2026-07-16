6fc11e86117e85715fbac935554488e0b7b7f712
ezerssss <magbanuaezra@gmail.com>
2026-07-16 22:18:46 +0800

    Caching and removal of unnecessary endpoint

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,18 +1,22 @@
 // @ts-check
-import { defineConfig } from 'astro/config';
+import { defineConfig } from "astro/config";
 
-import tailwindcss from '@tailwindcss/vite';
+import tailwindcss from "@tailwindcss/vite";
 
-import icon from 'astro-icon';
+import icon from "astro-icon";
 
-import vercel from '@astrojs/vercel';
+import vercel from "@astrojs/vercel";
+import { cacheVercel } from "@astrojs/vercel/cache";
 
 // https://astro.build/config
 export default defineConfig({
-  vite: {
-    plugins: [tailwindcss()]
-  },
+    vite: {
+        plugins: [tailwindcss()],
+    },
 
-  integrations: [icon()],
-  adapter: vercel()
+    integrations: [icon()],
+    adapter: vercel(),
+    cache: {
+        provider: cacheVercel(),
+    },
 });
