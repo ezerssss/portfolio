df2223f78a8a531d77f0eab33ec1006571d5860d
ezerssss <magbanuaezra@gmail.com>
2026-07-17 01:57:10 +0800

    Prerender on index

---
+++ src/constants/cache.ts
@@ -0,0 +1,5 @@
+// 2 weeks (60 secs * 60 mins * 24 hours * 14 days)
+export const cacheDuration = 60 * 60 * 24 * 14;
+
+// 1 hour (60 secs * 60 mins * 5 hours)
+export const staleWhileRevalidatingDuration = 60 * 60 * 1;
